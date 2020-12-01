fda8ed2cc3d0a6991f869c68b8688981db1a94ba
吕佐康 <lvzk@techsure.com.cn>
2020-12-01 18:44:34 +0800

    commit

---
+++ src/main/java/codedriver/framework/knowledge/App.java
@@ -0,0 +1,13 @@
+package codedriver.framework.knowledge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
