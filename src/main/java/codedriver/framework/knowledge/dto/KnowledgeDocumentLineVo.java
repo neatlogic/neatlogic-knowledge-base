d090c5c2fc47c58427cbcc47a552e9e984d04e71
linbq@techsure.com.cn <Besiter789>
2022-08-15 18:48:37 +0800

    --task=1006777 --user=林邦泉 后端-巡检-配置文件需求-文件查看与对比接口 https://www.tapd.cn/54247054/s/1246700

--- src/main/java/codedriver/framework/knowledge/dto/KnowledgeDocumentLineVo.java
+++ src/main/java/codedriver/framework/knowledge/dto/KnowledgeDocumentLineVo.java
@@ -1,25 +1,19 @@
 package codedriver.framework.knowledge.dto;
 
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.knowledge.linehandler.core.ILineHandler;
-import codedriver.framework.knowledge.linehandler.core.LineHandlerFactory;
+import codedriver.framework.lcs.BaseLineVo;
 import codedriver.framework.restful.annotation.EntityField;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.Objects;
-
-public class KnowledgeDocumentLineVo {
+public class KnowledgeDocumentLineVo extends BaseLineVo {
 
     @EntityField(name = "标题", type = ApiParamType.STRING)
     private String uuid;
-    private String handler;
-    private String content;
-    private String changeType;
-    private JSONObject config;
-    private Integer lineNumber;
+//    private String handler;
+//    private String content;
+//    private String changeType;
+//    private JSONObject config;
+//    private Integer lineNumber;
     @JSONField(serialize = false)
     private Long knowledgeDocumentId;
 //    @ESKey(type = ESKeyType.PKEY, name = "documentId")
@@ -29,14 +23,9 @@ public class KnowledgeDocumentLineVo {
     private String contentHash;
     @JSONField(serialize = false)
     private String configHash;
-    @JSONField(serialize = false)
-    private String configStr;
+//    @JSONField(serialize = false)
+//    private String configStr;
     public KnowledgeDocumentLineVo() {
-    }
-    public KnowledgeDocumentLineVo(Integer lineNumber, String handler, String content) {
-        this.lineNumber = lineNumber;
-        this.handler = handler;
-        this.content = content;
     }
     public String getUuid() {
         return uuid;
@@ -44,36 +33,36 @@ public class KnowledgeDocumentLineVo {
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }
-    public String getHandler() {
-        return handler;
-    }
-    public void setHandler(String handler) {
-        this.handler = handler;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public String getChangeType() {
-        return changeType;
-    }
-    public void setChangeType(String changeType) {
-        this.changeType = changeType;
-    }
-    public JSONObject getConfig() {
-        return config;
-    }
-    public void setConfig(String config) {
-        this.config = JSON.parseObject(config);
-    }
-    public Integer getLineNumber() {
-        return lineNumber;
-    }
-    public void setLineNumber(Integer lineNumber) {
-        this.lineNumber = lineNumber;
-    }
+//    public String getHandler() {
+//        return handler;
+//    }
+//    public void setHandler(String handler) {
+//        this.handler = handler;
+//    }
+//    public String getContent() {
+//        return content;
+//    }
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//    public String getChangeType() {
+//        return changeType;
+//    }
+//    public void setChangeType(String changeType) {
+//        this.changeType = changeType;
+//    }
+//    public JSONObject getConfig() {
+//        return config;
+//    }
+//    public void setConfig(String config) {
+//        this.config = JSON.parseObject(config);
+//    }
+//    public Integer getLineNumber() {
+//        return lineNumber;
+//    }
+//    public void setLineNumber(Integer lineNumber) {
+//        this.lineNumber = lineNumber;
+//    }
     public Long getKnowledgeDocumentId() {
         return knowledgeDocumentId;
     }
@@ -98,43 +87,43 @@ public class KnowledgeDocumentLineVo {
     public void setConfigHash(String configHash) {
         this.configHash = configHash;
     }
-    public String getConfigStr() {
-        if(StringUtils.isBlank(configStr) && config != null) {
-            configStr = config.toJSONString();
-        }
-        return configStr;
-    }
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((handler == null) ? 0 : handler.hashCode());
-        String mainBody = null;
-        ILineHandler lineHandler = LineHandlerFactory.getHandler(handler);
-        if (lineHandler != null) {
-            mainBody = lineHandler.getMainBody(this);
-        }
-        result = prime * result + ((mainBody == null) ? 0 : mainBody.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        KnowledgeDocumentLineVo other = (KnowledgeDocumentLineVo)obj;
-        if (handler == null) {
-            if (other.handler != null)
-                return false;
-        } else if (!handler.equals(other.handler))
-            return false;
-        ILineHandler lineHandler = LineHandlerFactory.getHandler(handler);
-        if (lineHandler == null) {
-            return true;
-        }
-        return Objects.equals(lineHandler.getMainBody(this), lineHandler.getMainBody(other));
-    }
+//    public String getConfigStr() {
+//        if(StringUtils.isBlank(configStr) && config != null) {
+//            configStr = config.toJSONString();
+//        }
+//        return configStr;
+//    }
+//    @Override
+//    public int hashCode() {
+//        final int prime = 31;
+//        int result = 1;
+//        result = prime * result + ((handler == null) ? 0 : handler.hashCode());
+//        String mainBody = null;
+//        ILineHandler lineHandler = LineHandlerFactory.getHandler(handler);
+//        if (lineHandler != null) {
+//            mainBody = lineHandler.getMainBody(this);
+//        }
+//        result = prime * result + ((mainBody == null) ? 0 : mainBody.hashCode());
+//        return result;
+//    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj)
+//            return true;
+//        if (obj == null)
+//            return false;
+//        if (getClass() != obj.getClass())
+//            return false;
+//        KnowledgeDocumentLineVo other = (KnowledgeDocumentLineVo)obj;
+//        if (handler == null) {
+//            if (other.handler != null)
+//                return false;
+//        } else if (!handler.equals(other.handler))
+//            return false;
+//        ILineHandler lineHandler = LineHandlerFactory.getHandler(handler);
+//        if (lineHandler == null) {
+//            return true;
+//        }
+//        return Objects.equals(lineHandler.getMainBody(this), lineHandler.getMainBody(other));
+//    }
 }
