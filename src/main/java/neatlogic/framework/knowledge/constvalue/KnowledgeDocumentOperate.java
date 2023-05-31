9457c1fc90f7697174807be4e90b91206b566ab7
lvzk <897706680@qq.com>
2023-05-31 10:36:01 +0800

    优化国际化翻译 采用json格式维护

--- src/main/java/neatlogic/framework/knowledge/constvalue/KnowledgeDocumentOperate.java
+++ src/main/java/neatlogic/framework/knowledge/constvalue/KnowledgeDocumentOperate.java
@@ -3,11 +3,11 @@ package neatlogic.framework.knowledge.constvalue;
 import neatlogic.framework.util.I18nUtils;
 
 public enum KnowledgeDocumentOperate {
-    SUBMIT("submit", "enum.knowledge.knowledgedocumentoperate.submit", "enum.knowledge.knowledgedocumentoperate.submit.1", false),
-    PASS("pass", "enum.knowledge.knowledgedocumentoperate.pass", "enum.knowledge.knowledgedocumentoperate.pass.1", false),
-    REJECT("reject", "enum.knowledge.knowledgedocumentoperate.reject", "enum.knowledge.knowledgedocumentoperate.reject.1", false),
-    SWITCHVERSION("switchversion", "enum.knowledge.knowledgedocumentoperate.switchversion", "enum.knowledge.knowledgedocumentoperate.switchversion.1", true),
-    DELETEVERSION("deleteversion", "enum.knowledge.knowledgedocumentoperate.deleteversion", "enum.knowledge.knowledgedocumentoperate.deleteversion.1", true);
+    SUBMIT("submit", "enum.knowledge.knowledgedocumentoperate.submit.a", "enum.knowledge.knowledgedocumentoperate.submit.b", false),
+    PASS("pass", "enum.knowledge.knowledgedocumentoperate.pass.a", "enum.knowledge.knowledgedocumentoperate.pass.b", false),
+    REJECT("reject", "enum.knowledge.knowledgedocumentoperate.reject.a", "enum.knowledge.knowledgedocumentoperate.reject.b", false),
+    SWITCHVERSION("switchversion", "enum.knowledge.knowledgedocumentoperate.switchversion.a", "enum.knowledge.knowledgedocumentoperate.switchversion.b", true),
+    DELETEVERSION("deleteversion", "enum.knowledge.knowledgedocumentoperate.deleteversion.a", "enum.knowledge.knowledgedocumentoperate.deleteversion.b", true);
     private String value;
     private String text;
     private String title;
