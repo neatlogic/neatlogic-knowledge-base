939d1de9b34e1f8f4035db88f23a15e3fb14cf23
1437892690@qq.com <1437892690@qq.com>
2026-06-02 11:56:37 +0800

    [功能] 飞书文档导入 neatlogic 知识库
    关联 #[1695041165459459]飞书文档导入 neatlogic 知识库 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1695041165459459

---
+++ src/main/java/neatlogic/framework/knowledge/dto/feishu/FeishuNode.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.framework.knowledge.dto.feishu;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeishuNode {
+    private final String nodeToken;
+    private final String objToken;
+    private final String objType;
+    private final String title;
+    private final String updateTime;
+    private final List<String> path = new ArrayList<>();
+
+    public FeishuNode(JSONObject item) {
+        this.nodeToken = item.getString("node_token");
+        this.objToken = item.getString("obj_token");
+        this.objType = item.getString("obj_type");
+        this.title = StringUtils.defaultIfBlank(item.getString("title"), item.getString("obj_token"));
+        this.updateTime = item.getString("obj_edit_time");
+    }
+
+    public String getNodeToken() {
+        return nodeToken;
+    }
+
+    public String getObjToken() {
+        return objToken;
+    }
+
+    public String getObjType() {
+        return objType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public List<String> getPath() {
+        return path;
+    }
+}
